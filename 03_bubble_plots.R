7a02f1e19715f1fa0cd54a5c189bfb2e77cf0833
Stella Fragkouli <79847870+sfragkoul@users.noreply.github.com>
2023-06-27 12:36:19 +0300

    Add visualizations

---
+++ 03_bubble_plots.R
@@ -0,0 +1,105 @@
+
+
+
+rm(list = ls())
+gc()
+
+library(data.table)
+library(stringr)
+
+df = fread("Ground_truth_vs_Mutect2.clean.annotated.tsv")
+
+df[which(df$`Mutect2 ALT` == "")]$`Mutect2 ALT` = NA
+
+library(ggplot2)
+library(ggforce)
+
+gr = ggplot(data = df) +
+    
+    geom_point(
+        aes(x = POS, y = `Ground Truth ALT`, size = `Ground Truth AF`, fill = "Ground Truth"),
+        position = position_nudge(y = .15), shape = 21, stroke = .25
+    ) +
+    
+    geom_point(
+        aes(x = POS, y = `Mutect2 ALT`, size = `Mutect2 AF`, fill = "Mutect2"),
+        position = position_nudge(y = -.15), shape = 21, stroke = .25
+    ) +
+    
+    scale_size_continuous(
+        range = c(2, 10), 
+        limits = c(0, 1),
+        breaks = c(.05, .1, .2, .5, .8),
+        labels = scales::percent,
+        guide = guide_legend(
+            title = "Allele Frequency",
+            title.position = "top"
+        )
+    ) +
+    
+    scale_fill_manual(
+        values = c(
+            "Ground Truth" = "#43ae8d",
+            "Mutect2"      = "#ae4364"
+        ),
+        
+        guide = guide_legend(
+            title = "Category",
+            title.position = "top",
+            
+            override.aes = list(size = 3.5)
+        )
+    ) +
+    
+    scale_x_continuous(labels = scales::comma, expand = c(0, 0),
+                       limits = c(0, 19500)) +
+    
+    theme_minimal() +
+    
+    theme(
+        legend.position = "bottom",
+        
+        axis.line.x = element_line(),
+        axis.ticks.x = element_line(),
+        
+        axis.text.y = element_text(face = "bold", size = 11),
+        
+        panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank(),
+        
+        plot.margin = margin(20, 20, 20, 20)
+    ) +
+    
+    labs(
+        x = "Chromosomal Position",
+        y = "Alterations"
+    )
+
+
+
+ggsave(
+    plot = gr, filename = "Bubble-plot.pdf", device = cairo_pdf,
+    width = 12, height = 6, units = "in"
+)
+
+ggsave(
+    plot = gr, filename = "Bubble-plot.svg",
+    width = 12, height = 6, units = "in"
+)
+
+ggsave(
+    plot = gr, filename = "Bubble-plot.jpeg",
+    width = 12, height = 6, units = "in", dpi = 600
+)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 03_density_plot.R
@@ -0,0 +1,108 @@
+
+
+
+rm(list = ls())
+gc()
+
+library(data.table)
+library(stringr)
+
+library(ggsci)
+
+df = fread("Ground_truth_vs_Mutect2.clean.annotated.tsv")
+
+df[which(df$`Mutect2 ALT` == "")]$`Mutect2 ALT` = NA
+
+
+df = df[, c(
+    "POS", 
+    "Ground Truth AF",
+    "Ground Truth ALT",
+    "Mutect2 ALT",
+    "Mutect2 AF"
+), with = FALSE] |>
+    unique()
+
+gr1 = ggplot(data = df[, 1:3], aes(x = `Ground Truth AF`)) +
+    
+    geom_density(aes(color = `Ground Truth ALT`, fill = `Ground Truth ALT`),
+                 alpha = .5) +
+    
+    scale_x_continuous(expand = c(0, 0), breaks = c(.25, .5, .75, 1), limits = c(0, 1), labels = scales::percent) +
+    scale_y_continuous(expand = c(0, 0)) +
+    
+    scale_fill_npg() +
+    scale_color_npg() +
+    
+    facet_wrap(vars(`Ground Truth ALT`), nrow = 1) +
+    
+    theme_minimal() +
+    
+    theme(
+        legend.position = "none",
+        
+        axis.title.x = element_blank(),
+        strip.text = element_text(face = "bold", size = 11),
+        
+        panel.spacing = unit(1, "lines"),
+        
+        panel.grid = element_line(linetype = "dashed")
+    ) +
+    
+    labs(y = "Ground Truth (density)")
+
+
+gr2 = ggplot(data = df[which(!is.na(`Mutect2 ALT`)), c(1, 4, 5)], aes(x = `Mutect2 AF`)) +
+    
+    geom_density(aes(color = `Mutect2 ALT`, fill = `Mutect2 ALT`),
+                 alpha = .5) +
+    
+    scale_x_continuous(expand = c(0, 0), breaks = c(.25, .5, .75, 1), limits = c(0, 1), labels = scales::percent) +
+    scale_y_continuous(expand = c(0, 0)) +
+    
+    scale_fill_npg() +
+    scale_color_npg() +
+    
+    facet_wrap(vars(`Mutect2 ALT`), nrow = 1) +
+    
+    theme_minimal() +
+    
+    theme(
+        legend.position = "none",
+        axis.title.x = element_text(face = "bold", size = 11),
+        strip.text = element_text(face = "bold", size = 11),
+        
+        panel.spacing = unit(1, "lines"),
+        
+        panel.grid = element_line(linetype = "dashed")
+    ) +
+    
+    labs(
+        x = "Allele Frequency",
+        y = "Mutect2 (density)"
+    )
+
+
+library(patchwork)
+
+multi = gr1 / gr2
+
+ggsave(
+    plot = multi, filename = "density-plot.pdf", device = cairo_pdf,
+    width = 12, height = 10, units = "in"
+)
+
+ggsave(
+    plot = multi, filename = "density-plot.svg",
+    width = 12, height = 10, units = "in"
+)
+
+ggsave(
+    plot = multi, filename = "density-plot.jpeg",
+    width = 12, height = 10, units = "in", dpi = 600
+)
+
+
+
+
+
