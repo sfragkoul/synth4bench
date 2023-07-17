e2a4ecd2a6f0bfbbeb3ea8a37f30ca0b3c16749b
sfragkoul <sfragkoul@certh.gr>
2023-07-17 11:26:52 +0300

    adjust paths in synth data generation script

--- runSynthDataGeneration.sh
+++ runSynthDataGeneration.sh
@@ -1,48 +1,48 @@
 #!/bin/sh
 echo  "Starting Run 1"
-mkdir 1_TP53
-python gen_reads.py -r testing/TP53/TP53.fasta -R 150 -c 500 -M 0.1 -o testing/TP53/1_TP53 --pe 300 30 --bam --vcf
+mkdir 1
+python gen_reads.py -r TP53.fasta -R 150 -c 500 -M 0.1 -o 1 --pe 300 30 --bam --vcf
 
 
 echo  "Starting Run 2"
-mkdir 2_TP53
-python gen_reads.py -r testing/TP53/TP53.fasta -R 150 -c 500 -M 0.1 -o testing/TP53/2_TP53 --pe 300 30 --bam --vcf
+mkdir 2
+python gen_reads.py -r TP53.fasta -R 150 -c 500 -M 0.1 -o 2 --pe 300 30 --bam --vcf
 
 
 echo  "Starting Run 3"
-mkdir 3_TP53
-python gen_reads.py -r testing/TP53/TP53.fasta -R 150 -c 500 -M 0.1 -o testing/TP53/3_TP53 --pe 300 30 --bam --vcf
+mkdir 3
+python gen_reads.py -r TP53.fasta -R 150 -c 500 -M 0.1 -o 3 --pe 300 30 --bam --vcf
 
 
 echo  "Starting Run 4"
-mkdir 4_TP53
-python gen_reads.py -r testing/TP53/TP53.fasta -R 150 -c 500 -M 0.1 -o testing/TP53/4_TP53 --pe 300 30 --bam --vcf
+mkdir 4
+python gen_reads.py -r TP53.fasta -R 150 -c 500 -M 0.1 -o 4 --pe 300 30 --bam --vcf
 
 echo  "Starting Run 5"
-mkdir 5_TP53
-python gen_reads.py -r testing/TP53/TP53.fasta -R 150 -c 500 -M 0.1 -o testing/TP53/5_TP53 --pe 300 30 --bam --vcf
+mkdir 5
+python gen_reads.py -r TP53.fasta -R 150 -c 500 -M 0.1 -o 5 --pe 300 30 --bam --vcf
 
 
 echo  "Starting Run 6"
-mkdir 6_TP53
-python gen_reads.py -r testing/TP53/TP53.fasta -R 150 -c 500 -M 0.1 -o testing/TP53/6_TP53 --pe 300 30 --bam --vcf
+mkdir 6
+python gen_reads.py -r TP53.fasta -R 150 -c 500 -M 0.1 -o 6 --pe 300 30 --bam --vcf
 
 
 echo  "Starting Run 7"
-mkdir 7_TP53
-python gen_reads.py -r testing/TP53/TP53.fasta -R 150 -c 500 -M 0.1 -o testing/TP53/7_TP53 --pe 300 30 --bam --vcf
+mkdir 7
+python gen_reads.py -r TP53.fasta -R 150 -c 500 -M 0.1 -o 7 --pe 300 30 --bam --vcf
 
 
 echo  "Starting Run 8"
-mkdir 8_TP53
-python gen_reads.py -r testing/TP53/TP53.fasta -R 150 -c 500 -M 0.1 -o testing/TP53/8_TP53 --pe 300 30 --bam --vcf
+mkdir 8
+python gen_reads.py -r TP53.fasta -R 150 -c 500 -M 0.1 -o 8 --pe 300 30 --bam --vcf
 
 
 echo  "Starting Run 9"
-mkdir 9_TP53
-python gen_reads.py -r testing/TP53/TP53.fasta -R 150 -c 500 -M 0.1 -o testing/TP53/9_TP53 --pe 300 30 --bam --vcf
+mkdir 9
+python gen_reads.py -r TP53.fasta -R 150 -c 500 -M 0.1 -o 9 --pe 300 30 --bam --vcf
 
 
 echo  "Starting Run 10"
-mkdir 10_TP53
-python gen_reads.py -r testing/TP53/TP53.fasta -R 150 -c 500 -M 0.1 -o testing/TP53/10_TP53 --pe 300 30 --bam --vcf
+mkdir 10
+python gen_reads.py -r TP53.fasta -R 150 -c 500 -M 0.1 -o 10 --pe 300 30 --bam --vcf
