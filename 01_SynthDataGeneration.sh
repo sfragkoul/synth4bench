624e0b898a1a6028c4ddc635da9c486ed3270376
Stella Fragkouli <79847870+sfragkoul@users.noreply.github.com>
2023-09-08 11:22:55 +0300

    Update 01_SynthDataGeneration.sh

--- 01_SynthDataGeneration.sh
+++ 01_SynthDataGeneration.sh
@@ -58,17 +58,17 @@ python gen_reads.py -r TP53.fasta -R 150 -c 500 -M 0.1 -o 10 --pe 300 30 --bam -
 echo  "Merging bam files"
 samtools merge Merged2.bam 1/1_golden.bam 2/2_golden.bam 3/3_golden.bam 4/4_golden.bam 5/5_golden.bam 6/6_golden.bam 7/7_golden.bam 8/8_golden.bam 9/9_golden.bam 10/10_golden.bam
 
-echo  "Merging vcf files"
-bcftools index 1/1_golden.vcf.gz
-bcftools index 2/2_golden.vcf.gz
-bcftools index 3/3_golden.vcf.gz
-bcftools index 4/4_golden.vcf.gz
-bcftools index 5/5_golden.vcf.gz
-bcftools index 6/6_golden.vcf.gz
-bcftools index 7/7_golden.vcf.gz
-bcftools index 8/8_golden.vcf.gz
-bcftools index 9/9_golden.vcf.gz
-bcftools index 10/10_golden.vcf.gz
+#echo  "Merging vcf files"
+#bcftools index 1/1_golden.vcf.gz
+#bcftools index 2/2_golden.vcf.gz
+#bcftools index 3/3_golden.vcf.gz
+#bcftools index 4/4_golden.vcf.gz
+#bcftools index 5/5_golden.vcf.gz
+#bcftools index 6/6_golden.vcf.gz
+#bcftools index 7/7_golden.vcf.gz
+#bcftools index 8/8_golden.vcf.gz
+#bcftools index 9/9_golden.vcf.gz
+#bcftools index 10/10_golden.vcf.gz
 
-bcftools merge 1/1_golden.vcf.gz 2/2_golden.vcf.gz 3/3_golden.vcf.gz 4/4_golden.vcf.gz 5/5_golden.vcf.gz 6/6_golden.vcf.gz 7/7_golden.vcf.gz 8/8_golden.vcf.gz 9/9_golden.vcf.gz 10/10_golden.vcf.gz > Merged.vcf
+#bcftools merge 1/1_golden.vcf.gz 2/2_golden.vcf.gz 3/3_golden.vcf.gz 4/4_golden.vcf.gz 5/5_golden.vcf.gz 6/6_golden.vcf.gz 7/7_golden.vcf.gz 8/8_golden.vcf.gz 9/9_golden.vcf.gz 10/10_golden.vcf.gz > Merged.vcf
  
