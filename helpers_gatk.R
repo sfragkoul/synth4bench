445c9ac70122e547c42bb90e412e085a32a0dd48
sfragkoul <sfragkoul@certh.gr>
2024-02-27 12:31:11 +0200

    add gatk plot helpers

--- helpers_gatk.R
+++ helpers_gatk.R
@@ -216,3 +216,332 @@ clean_gatk <- function(df) {
     return(df2)
     
 }
+
+bar_plots_gatk <- function(q) {
+    
+    q[which(q$`Mutect2 ALT` == "")]$`Mutect2 ALT` = NA
+    
+    # plot 1 ------------------------
+    
+    df = q[, c(
+        "POS", 
+        "Ground Truth DP",
+        "Mutect2 DP"
+    ), with = FALSE] |>
+        unique() |>
+        
+        melt(id.vars = "POS", variable.factor = FALSE, value.factor = FALSE)
+    
+    o1 = ggplot(data = df) +
+        
+        geom_point(aes(x = variable, y = value, fill = variable),
+                   position = position_jitternormal(sd_x = .01, sd_y = 0),
+                   shape = 21, stroke = .1, size = 2.5) +
+        
+        geom_boxplot(aes(x = variable, y = value, fill = variable),
+                     width = .25, alpha = .5, outlier.shape = NA) +
+        
+        scale_fill_manual(
+            values = c(
+                "Ground Truth DP" = "#43ae8d",
+                "Mutect2 DP"      = "#ae4364"
+            )
+        ) +
+        
+        scale_x_discrete(
+            breaks = c("Ground Truth DP", "Mutect2 DP"),
+            labels = c("Ground Truth", "Mutect2")
+        ) +
+        
+        scale_y_continuous(labels = scales::comma) +
+        
+        theme_minimal() +
+        
+        theme(
+            legend.position = "none",
+            
+            axis.title.x = element_blank(),
+            axis.title.y = element_text(face = "bold", size = 13),
+            axis.text.x = element_text(face = "bold", size = 13),
+            axis.text.y = element_text(face = "bold", size = 13),
+            
+            axis.line = element_line(),
+            axis.ticks = element_line(),
+            
+            panel.grid = element_blank(),
+            
+            plot.margin = margin(20, 20, 20, 20)
+        ) +
+        
+        labs(
+            y = "Coverage (No. of reads)"
+        )
+    
+    
+    # plot 2 ---------------------
+    
+    df = q[, c(
+        "POS",
+        "Ground Truth AF",
+        "Mutect2 AF"
+    ), with = FALSE] |>
+        unique() |>
+        
+        melt(id.vars = "POS", variable.factor = FALSE, value.factor = FALSE)
+    
+    o2 = ggplot(data = df[which(!is.na(value) & value != 0)]) +
+        
+        geom_point(aes(x = variable, y = value, fill = variable),
+                   position = position_jitternormal(sd_x = .01, sd_y = 0),
+                   shape = 21, stroke = .1, size = 2.5) +
+        
+        geom_boxplot(aes(x = variable, y = value, fill = variable),
+                     width = .25, alpha = .5, outlier.shape = NA) +
+        
+        scale_fill_manual(
+            values = c(
+                "Ground Truth AF" = "#43ae8d",
+                "Mutect2 AF"      = "#ae4364"
+            )
+        ) +
+        
+        scale_x_discrete(
+            breaks = c("Ground Truth AF", "Mutect2 AF"),
+            labels = c("Ground Truth", "Mutect2")
+        ) +
+        
+        scale_y_continuous(labels = scales::percent, trans = "log10") +
+        
+        theme_minimal() +
+        
+        theme(
+            legend.position = "none",
+            
+            axis.title.x = element_blank(),
+            axis.title.y = element_text(face = "bold", size = 13),
+            axis.text.x = element_text(face = "bold", size = 13),
+            axis.text.y = element_text(face = "bold", size = 13),
+            
+            axis.line = element_line(),
+            axis.ticks = element_line(),
+            
+            panel.grid = element_blank(),
+            
+            plot.margin = margin(20, 20, 20, 20)
+        ) +
+        
+        labs(
+            y = "Allele Frequency"
+        )
+    
+    # return -------------
+    
+    return(
+        list(
+            "coverage" = o1,
+            "allele" = o2
+        )
+    )
+    
+}
+
+density_plot_gatk <- function(q) {
+    
+    q[which(df$`Mutect2 ALT` == "")]$`Mutect2 ALT` = NA
+    
+    df = q[, c(
+        "POS", 
+        "Ground Truth AF",
+        "Ground Truth ALT",
+        "Mutect2 ALT",
+        "Mutect2 AF"
+    ), with = FALSE] |>
+        unique()
+    
+    # plot 1 ---------------------------
+    
+    o1 = ggplot(data = df[, 1:3], aes(x = `Ground Truth AF`)) +
+        
+        geom_density(aes(color = `Ground Truth ALT`, fill = `Ground Truth ALT`),
+                     alpha = .5) +
+        
+        scale_x_continuous(expand = c(0, 0), breaks = c(.25, .5, .75, 1), limits = c(0, 1), labels = scales::percent) +
+        scale_y_continuous(expand = c(0, 0), limits = c(0, 15), breaks = c(5, 10, 15)) +
+        
+        scale_fill_npg() +
+        scale_color_npg() +
+        
+        facet_wrap(vars(`Ground Truth ALT`), nrow = 1) +
+        
+        theme_minimal() +
+        
+        theme(
+            legend.position = "none",
+            
+            axis.title.x = element_blank(),
+            axis.title.y = element_text(face = "bold", size = 13),
+            strip.text = element_text(face = "bold", size = 13),
+            axis.text.y = element_text(face = "bold", size = 13),
+            axis.text.x = element_blank(),
+            
+            panel.spacing = unit(1, "lines"),
+            
+            panel.grid = element_line(linetype = "dashed")
+        ) +
+        
+        labs(y = "Ground Truth (density)")
+    
+    # plot 2 ----------------------
+    
+    o2 = ggplot(data = df[which(!is.na(`Mutect2 ALT`)), c(1, 4, 5)], aes(x = `Mutect2 AF`)) +
+        
+        geom_density(aes(color = `Mutect2 ALT`, fill = `Mutect2 ALT`),
+                     alpha = .5) +
+        
+        scale_x_continuous(expand = c(0, 0), breaks = c(.25, .5, .75, 1), limits = c(0, 1), labels = scales::percent) +
+        scale_y_continuous(expand = c(0, 0), limits = c(0, 15), breaks = c(5, 10, 15))+
+        
+        scale_fill_npg() +
+        scale_color_npg() +
+        
+        facet_wrap(vars(`Mutect2 ALT`), nrow = 1) +
+        
+        theme_minimal() +
+        
+        theme(
+            legend.position = "none",
+            axis.title.x = element_text(face = "bold", size = 13),
+            axis.title.y = element_text(face = "bold", size = 13),
+            strip.text = element_blank(), # element_text(face = "bold", size = 13),
+            axis.text.y = element_text(face = "bold", size = 13),
+            axis.text.x = element_text(face = "bold", size = 11),
+            
+            panel.spacing = unit(1, "lines"),
+            
+            panel.grid = element_line(linetype = "dashed")
+        ) +
+        
+        labs(
+            x = "Allele Frequency",
+            y = "Mutect2 (density)"
+        )
+    
+    
+    # return -----------------
+    
+    return(
+        list(
+            "groundtruth" = o1,
+            "mutect2" = o2
+        )
+    )
+    
+}
+
+bubble_plots_gatk <- function(q) {
+    
+    # q[which(q$`Mutect2 ALT` == "")]$`Mutect2 ALT` = NA
+    
+    
+    q1 = q[which(q$`Mutect2 ALT` != "")]
+    
+    
+    o = ggplot() +
+        
+        geom_point(
+            data = q,
+            aes(x = POS, y = `Ground Truth ALT`, size = `Ground Truth AF`, fill = "Ground Truth"),
+            position = position_nudge(y = .15), shape = 21, stroke = .25
+        ) +
+        
+        geom_point(
+            data = q1,
+            aes(x = POS, y = `Mutect2 ALT`, size = `Mutect2 AF`, fill = "Mutect2"),
+            position = position_nudge(y = -.15), shape = 21, stroke = .25
+        ) +
+        
+        scale_size_continuous(
+            range = c(2, 10), 
+            limits = c(0, 1),
+            breaks = c(.05, .1, .2, .5, .8),
+            labels = scales::percent,
+            guide = guide_legend(
+                title = "Allele Frequency",
+                title.position = "top"
+            )
+        ) +
+        
+        scale_fill_manual(
+            values = c(
+                "Ground Truth" = "#43ae8d",
+                "Mutect2"      = "#ae4364"
+            ),
+            
+            guide = guide_legend(
+                title = "Category",
+                title.position = "top",
+                
+                override.aes = list(size = 3.5)
+            )
+        ) +
+        
+        scale_x_continuous(labels = scales::comma_format(suffix = " bp"), 
+                           expand = c(0, 0),
+                           limits = c(0, 20000)) +
+        
+        scale_y_discrete(breaks = c("A", "C", "G", "T")) +
+        
+        theme_minimal() +
+        
+        theme(
+            legend.position = "bottom",
+            
+            axis.line.x = element_line(),
+            axis.ticks.x = element_line(),
+            
+            axis.text.y = element_text(face = "bold", size = 13),
+            axis.text.x = element_text(face = "bold", size = 11),
+            axis.title.y = element_text(face = "bold", size = 13),
+            axis.title.x = element_text(face = "bold", size = 13),
+            
+            panel.grid.minor = element_blank(),
+            panel.grid.major.x = element_blank(),
+            
+            plot.margin = margin(20, 30, 20, 20)
+        ) +
+        
+        labs(
+            x = "Chromosomal Position",
+            y = "Alterations"
+        )
+    
+    return(o)
+    
+}
+
+venn_plot_gatk <- function(q, p) {
+    
+    vcf_GT = vcfR::getFIX(q) |> as.data.frame() |> setDT()
+    vcf_GT$scenario = "GT"
+    
+    vcf_gatk = vcfR::getFIX(p) |> as.data.frame() |> setDT()
+    vcf_gatk$scenario = "GATK"
+    
+    x = rbind(vcf_GT, vcf_gatk)
+    y = x[, c("CHROM", "POS", "REF", "ALT", "scenario"), with = FALSE]
+    
+    y$mut = paste(y$CHROM, y$POS, y$REF, y$ALT, sep = ":")
+    
+    y = split(y, y$scenario)
+    
+    y = list(
+        'Ground Truth' = y$GT$mut,
+        'GATK'         = y$GATK$mut
+    )
+    
+    gr = ggvenn(y, fill_color = c("#43ae8d", "#ae4364")) +
+        
+        coord_equal(clip = "off")
+    
+    return(gr)
+}
