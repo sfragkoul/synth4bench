24e4b3dc522baa387ff268e341f14f01956f87ae
sfragkoul <sfragkoul@certh.gr>
2024-07-17 14:16:14 +0300

    add sam2tsv file

--- R/sam2tsv_try.R
+++ R/sam2tsv_try.R
@@ -1,3 +1,5 @@
+#jvarkit sam2tsv /mnt/c/Users/sfragkoul/Desktop/synth4bench_Copy/results/Merged_auto.sorted.uniq.rg.bam -o  /mnt/c/Users/sfragkoul/Desktop/synth4bench_Copy/results/sam2tsv.tsv -R /mnt/d/sfragkoul/Synth_Data/Synthesizers/NEAT/testing/TP53/TP53.fasta
+
 library(data.table)
 
 data <- fread("sam2tsv.tsv")
