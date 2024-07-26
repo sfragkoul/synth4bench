8aa3b60c2021b9845a308da55cc47adc4f2a06c3
sfragkoul <sfragkoul@certh.gr>
2024-07-26 14:34:33 +0300

    violin & river plots

--- R/issue_1.R
+++ R/issue_1.R
@@ -92,9 +92,9 @@ define_tp <- function(caller, gt){
     return(tp_var)
 }
 
-nt_runs = load_gt_report("results/", "Merged_auto")
+gt_runs = load_gt_report("results/", "Merged_auto")
 # select SNVs
-nt_runs = nt_runs[which(ALT %in% c("A", "C", "G", "T")), ]
+nt_runs = gt_runs[which(ALT %in% c("A", "C", "G", "T")), ]
 #filter DEPTH>2
 nt_runs = nt_runs[which(nt_runs$Count >2), ]
 pick_gt = load_gt_vcf("results/", "Merged_auto")
@@ -104,7 +104,7 @@ fn_dp_barplot <- function(q, caller){
   #FP DP plot
   df = q[, c(
     "POS", 
-    "DP"
+    "Ground Truth DP"
   ), with = FALSE] |>
     unique() |>
     melt(id.vars = "POS", variable.factor = FALSE, value.factor = FALSE)
@@ -120,7 +120,7 @@ fn_dp_barplot <- function(q, caller){
     
     scale_fill_manual(
       values = c(
-        "DP" = "#43ae8d"
+        "Ground Truth DP" = "#43ae8d"
       )
     ) +
     
@@ -159,7 +159,7 @@ fn_af_barplot <- function(q, caller){
   #FP AF plot
   df = q[, c(
     "POS",
-    "Freq"
+    "Ground Truth AF"
   ), with = FALSE] |>
     unique() |>
     
@@ -176,7 +176,7 @@ fn_af_barplot <- function(q, caller){
     
     scale_fill_manual(
       values = c(
-        "Freq" = "#43ae8d"
+        "Ground Truth AF" = "#43ae8d"
       )
     ) +
     
@@ -229,72 +229,172 @@ load_gatk_vcf <- function(path, merged_file){
 Mutect2_somatic <- load_gatk_vcf("results/", "Merged_auto")
 Mutect2_somatic_snvs <-select_snvs(Mutect2_somatic)
 
+#FP
 fp_var = define_fp(Mutect2_somatic_snvs, pick_gt)
 fp_var$gt_AF = as.numeric(fp_var$gt_AF)
+colnames(fp_var) = c("CHROM", "POS","ID", "Mutect2 REF",	
+                                   "Mutect2 ALT", "Mutect2 QUAL",	"Mutect2 FILTER",
+                                   "key", "Indiv", "Mutect2 AD", "Mutect2 AF",
+                                   "Mutect2 DP", "gt_F1R2", "gt_F2R1", "gt_FAD",	
+                                   "gt_GQ", "gt_GT",	"gt_PGT",	"gt_PID",	"gt_PL",
+                                   "gt_PS",	"gt_SB",	"gt_GT_alleles", "mut")
+
+teeeeest = gt_runs[which(POS %in% unique(fp_var$POS))]
+a = unique(teeeeest, by = "POS")
+fp_var$`Ground Truth DP` = a$DP
+fp_var$`DP Percentage` = fp_var$`Mutect2 DP`/fp_var$`Ground Truth DP`
+
+
+
+# Plot the alluvial diagram
+ggplot(data = fp_var,
+       aes(axis1 = `Mutect2 DP`, axis2 = `Ground Truth DP`)) +
+    geom_alluvium(aes(fill = `Mutect2 DP`), width = 0.1) +
+    geom_stratum(width = 0.1, fill = "grey", color = "black") +
+    geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
+    scale_x_discrete(limits = c("Mutect2", "Ground Truth"), expand = c(0.15, 0.05)) +
+    theme_minimal() +
+    labs(title = "Alluvial Diagram",
+         x = "Dimensions")
+
+
+
+
+
+#FN
 fn_var = define_fn(Mutect2_somatic_snvs, pick_gt)
+colnames(fn_var) = c("POS", "Ground Truth REF", "Ground Truth DP", 
+                "Ground Truth ALT", "Count", "Ground Truth AF", "mut")
+
+
 tp_var = define_tp(Mutect2_somatic_snvs, pick_gt)
 
-fp_dp_barplot <- function(q ){
-  #FP DP plot
-  df = q[, c(
-    "POS", 
-    "gt_DP"
-  ), with = FALSE] |>
-    unique() |>
-    melt(id.vars = "POS", variable.factor = FALSE, value.factor = FALSE)
-  
-  o1=ggplot(data = df) +
-    
-    geom_point(aes(x = variable, y = value, fill = variable),
-               position = position_jitternormal(sd_x = .01, sd_y = 0),
-               shape = 21, stroke = .1, size = 2.5) +
-    
-    geom_boxplot(aes(x = variable, y = value, fill = variable),
-                 width = .25, alpha = .5, outlier.shape = NA) +
-    
-    scale_fill_manual(
-      values = c(
-       # "Ground Truth DP" = "#43ae8d",
-        "gt_DP"      = "#ae4364"
-      )
-    ) +
-    
-    scale_x_discrete(
-      labels = c("Mutect2 FP Variants")
-    ) +
-    
-    scale_y_continuous(labels = scales::comma) +
-    
-    theme_minimal() +
-    
-    theme(
-      legend.position = "none",
-      
-      axis.title.x = element_blank(),
-      axis.title.y = element_text(face = "bold", size = 13),
-      
-      axis.text.x = element_text(face = "bold", size = 13),
-      axis.text.y = element_text(face = "bold", size = 13),
-      
-      axis.line = element_line(),
-      axis.ticks = element_line(),
-      
-      panel.grid = element_blank(),
-      
-      plot.margin = margin(20, 20, 20, 20)
-    ) +
-    
-    labs(
-      y = "Coverage (No. of reads)"
-    )
-  return(o1)
+# fp_dp_barplot <- function(q ){
+#   #FP DP plot
+#   df = q[, c(
+#     "POS", 
+#     "Mutect2 DP"
+#   ), with = FALSE] |>
+#     unique() |>
+#     melt(id.vars = "POS", variable.factor = FALSE, value.factor = FALSE)
+#   
+#   o1=ggplot(data = df) +
+#     
+#     geom_point(aes(x = variable, y = value, fill = variable),
+#                position = position_jitternormal(sd_x = .01, sd_y = 0),
+#                shape = 21, stroke = .1, size = 2.5) +
+#     
+#     geom_boxplot(aes(x = variable, y = value, fill = variable),
+#                  width = .25, alpha = .5, outlier.shape = NA) +
+#     
+#     scale_fill_manual(
+#       values = c(
+#        # "Ground Truth DP" = "#43ae8d",
+#         "Mutect2 DP"      = "#ae4364"
+#       )
+#     ) +
+#     
+#     scale_x_discrete(
+#       labels = c("Mutect2 FP Variants")
+#     ) +
+#     
+#     scale_y_continuous(labels = scales::comma) +
+#     
+#     theme_minimal() +
+#     
+#     theme(
+#       legend.position = "none",
+#       
+#       axis.title.x = element_blank(),
+#       axis.title.y = element_text(face = "bold", size = 13),
+#       
+#       axis.text.x = element_text(face = "bold", size = 13),
+#       axis.text.y = element_text(face = "bold", size = 13),
+#       
+#       axis.line = element_line(),
+#       axis.ticks = element_line(),
+#       
+#       panel.grid = element_blank(),
+#       
+#       plot.margin = margin(20, 20, 20, 20)
+#     ) +
+#     
+#     labs(
+#       y = "Coverage (No. of reads)"
+#     )
+#   return(o1)
+# 
+# }
 
+
+fp_violin_plots_gatk <- function(q) {
+    #function to produce variants' barplots for coverage and AF
+    #q[which(q$`Mutect2 ALT` == "")]$`Mutect2 ALT` = NA
+    q$POS = as.numeric(q$POS)
+    q$`Ground Truth DP` = as.numeric(q$`Ground Truth DP`)
+    q$`Mutect2 DP` = as.numeric(q$`Mutect2 DP`)
+    
+    #DP plot
+    df = q[, c(
+        "POS", 
+        "Ground Truth DP",
+        "Mutect2 DP"
+    ), with = FALSE] |>
+        unique() |>
+        
+        melt(id.vars = "POS", variable.factor = FALSE, value.factor = FALSE)
+    
+    o1 = ggplot(data = df) +
+        
+        geom_point(aes(x = variable, y = value, fill = variable),
+                   position = position_jitternormal(sd_x = .01, sd_y = 0),
+                   shape = 21, stroke = .1, size = 2.5) +
+        
+        geom_violin(aes(x = variable, y = value, fill = variable),
+                     width = .25, alpha = .5, outlier.shape = NA) +
+        
+        scale_fill_manual(
+            values = c(
+                "Ground Truth DP" = "#43ae8d",
+                "Mutect2 DP"      = "#ae4364"
+            )
+        ) +
+        
+        scale_x_discrete(
+            breaks = c("Ground Truth DP", "Mutect2 DP"),
+            labels = c("Ground Truth", "Mutect2")
+        ) +
+        
+        scale_y_continuous(labels = scales::comma) +
+        
+        theme_minimal() +
+        
+        theme(
+            legend.position = "none",
+            
+            axis.title.x = element_blank(),
+            axis.title.y = element_text(face = "bold", size = 13),
+            axis.text.x = element_text(face = "bold", size = 13),
+            axis.text.y = element_text(face = "bold", size = 13),
+            
+            axis.line = element_line(),
+            axis.ticks = element_line(),
+            
+            panel.grid = element_blank(),
+            
+            plot.margin = margin(20, 20, 20, 20)
+        ) +
+        
+        labs(
+            y = "Coverage (No. of reads)"
+        )
 }
+
 fp_af_barplot <- function(q){
   #FP AF plot
   df = q[, c(
     "POS",
-    "gt_AF"
+    "Mutect2 AF"
   ), with = FALSE] |>
     unique() |>
     
@@ -312,7 +412,7 @@ fp_af_barplot <- function(q){
     scale_fill_manual(
       values = c(
         #"Ground Truth AF" = "#43ae8d",
-        "gt_AF"      = "#ae4364"
+        "Mutect2 AF"      = "#ae4364"
       )
     ) +
     
@@ -347,7 +447,7 @@ fp_af_barplot <- function(q){
   
 }
 
-gatk_fp_plot1 <- fp_dp_barplot(fp_var)
+gatk_fp_plot1 <- fp_violin_plots_gatk(fp_var)
 gatk_fp_plot2 <- fp_af_barplot(fp_var)
 gatk_fn_plot1 <- fn_dp_barplot(fn_var, caller = "Mutect2")
 gatk_fn_plot2 <- fn_af_barplot(fn_var, caller = "Mutect2")
