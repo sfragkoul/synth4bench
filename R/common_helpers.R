5ffabd1d99a31313e46ece94647721775538e296
sfragkoul <sfragkoul@certh.gr>
2024-07-31 12:17:10 +0300

    move functions to common_helpers.R

--- R/common_helpers.R
+++ R/common_helpers.R
@@ -1,5 +1,6 @@
 
 
+#TP SNVS
 gt_analysis <- function(runs, folder, merged_file) {
   
   nt_runs = list()
@@ -465,5 +466,215 @@ explore_mut_pos <- function(runs, folder, caller) {
     out = rbind(found, not_found)
     
     return(out)
+    
+}
+
+#FP & FN SNVS
+#function "not in" def
+`%ni%` <- Negate(`%in%`) 
+
+load_gt_report <- function(path, merged_file) {
+    #function to load Ground Truth bam-report 
+    a <- paste0(path, merged_file, "_report.tsv") |>
+        readLines() |>
+        str_split(pattern = "\t", simplify = TRUE) |>
+        as.data.frame() |> 
+        setDT()
+    
+    a$V1 = NULL
+    a$V5 = NULL
+    
+    colnames(a) = c("POS", "REF", "DP", paste0("ALT_", 1:(ncol(a) - 3)))
+    
+    a = melt(
+        a, id.vars = c("POS", "REF", "DP"),
+        variable.factor = FALSE, value.factor = FALSE,
+        variable.name = "ALT", value.name = "Count"
+    )
+    
+    a = a[which(Count != "")]
+    
+    a$POS = as.numeric(a$POS)
+    a$DP = as.numeric(a$DP)
+    
+    a$ALT = str_split_i(a$Count, "\\:", 1)
+    
+    a$Count = str_split_i(a$Count, "\\:", 2) |>
+        as.numeric()
+    
+    a$Freq = round(a$Count / a$DP, digits = 6)
+    
+    a = a[order(POS, -Count)]
+    
+    a = a[which(REF != a$ALT & Count != 0)]
+    
+    # select SNVs
+    a_snvs = a[which(ALT %in% c("A", "C", "G", "T")), ]
+    #filter DEPTH>2
+    a_snvs = a_snvs[which(a_snvs$Count >2), ]
+    
+    
+    gt = list(
+        all = a,
+        snvs = a_snvs
+        
+    )
+    return(gt)
+}
+
+load_gt_vcf <- function(path, merged_file){
+    #function to load Ground Truth vcf
+    ground_truth_vcf <- read.vcfR( paste0(path, merged_file, 
+                                          "_ground_truth_norm.vcf"),
+                                   verbose = FALSE )
+    
+    ground_truth_vcf  = ground_truth_vcf |> vcfR::getFIX() |> as.data.frame() |> setDT()
+    
+    pick_gt = gt_snvs[which(gt_snvs$POS %in% ground_truth_vcf$POS)]
+    pick_gt$mut = paste(pick_gt$POS, 
+                        pick_gt$REF, 
+                        pick_gt$ALT, sep = ":")
+    return(pick_gt)
+}
+
+select_snvs <- function(df){
+    # select SNVs from caller based on length of REF and ALT
+    snvs = df[nchar(df$REF) == nchar(df$ALT)]
+    snvs = snvs[which(nchar(snvs$REF) <2), ]
+    snvs$mut = paste(snvs$POS, snvs$REF, snvs$ALT, sep = ":")
+    
+    return(snvs)
+}
+
+define_fp <- function(caller, gt){
+    #FP Variants
+    fp_var = caller[which(caller$mut %ni% gt$mut)]
+    
+    return(fp_var)
+}
+
+define_fn <- function(caller, gt){
+    #FN Variants
+    fn_var = gt[which(gt$mut %ni% caller$mut)]
+    fn_var$type = "FN"
+    
+    return(fn_var)
+}
+
+define_tp <- function(caller, gt){
+    #FN Variants
+    tp_var = caller[which(caller$mut %in% gt$mut)]
+    
+    return(tp_var)
+}
+
+fn_dp_barplot <- function(q, caller){
+    #FP DP plot
+    df = q[, c(
+        "POS", 
+        "Ground Truth DP"
+    ), with = FALSE] |>
+        unique() |>
+        melt(id.vars = "POS", variable.factor = FALSE, value.factor = FALSE)
+    
+    o3=ggplot(data = df) +
+        
+        geom_point(aes(x = variable, y = value, fill = variable),
+                   position = position_jitternormal(sd_x = .01, sd_y = 0),
+                   shape = 21, stroke = .1, size = 2.5) +
+        
+        geom_boxplot(aes(x = variable, y = value, fill = variable),
+                     width = .25, alpha = .5, outlier.shape = NA) +
+        
+        scale_fill_manual(
+            values = c(
+                "Ground Truth DP" = "#43ae8d"
+            )
+        ) +
+        
+        scale_x_discrete(
+            labels = c(paste0(caller, " FN Variants"))
+        ) +
+        
+        scale_y_continuous(labels = scales::comma) +
+        
+        theme_minimal() +
+        
+        theme(
+            legend.position = "none",
+            
+            axis.title.x = element_blank(),
+            axis.title.y = element_text(face = "bold", size = 13),
+            
+            axis.text.x = element_text(face = "bold", size = 13),
+            axis.text.y = element_text(face = "bold", size = 13),
+            
+            axis.line = element_line(),
+            axis.ticks = element_line(),
+            
+            panel.grid = element_blank(),
+            
+            plot.margin = margin(20, 20, 20, 20)
+        ) +
+        
+        labs(
+            y = "Coverage (No. of reads)"
+        )
+    return(o3)
+    
+}
+fn_af_barplot <- function(q, caller){
+    #FP AF plot
+    df = q[, c(
+        "POS",
+        "Ground Truth AF"
+    ), with = FALSE] |>
+        unique() |>
+        
+        melt(id.vars = "POS", variable.factor = FALSE, value.factor = FALSE)
+    
+    o4 = ggplot(data = df[which(!is.na(value) & value != 0)]) +
+        
+        geom_point(aes(x = variable, y = value, fill = variable),
+                   position = position_jitternormal(sd_x = .01, sd_y = 0),
+                   shape = 21, stroke = .1, size = 2.5) +
+        
+        geom_boxplot(aes(x = variable, y = value, fill = variable),
+                     width = .25, alpha = .5, outlier.shape = NA) +
+        
+        scale_fill_manual(
+            values = c(
+                "Ground Truth AF" = "#43ae8d"
+            )
+        ) +
+        
+        scale_x_discrete(
+            labels = c(paste0(caller, " FN Variants"))
+        ) +
+        
+        scale_y_continuous(labels = scales::percent, trans = "log10") +
+        
+        theme_minimal() +
+        
+        theme(
+            legend.position = "none",
+            
+            axis.title.x = element_blank(),
+            axis.title.y = element_text(face = "bold", size = 13),
+            axis.text.x = element_text(face = "bold", size = 13),
+            axis.text.y = element_text(face = "bold", size = 13),
+            
+            axis.line = element_line(),
+            axis.ticks = element_line(),
+            
+            panel.grid = element_blank(),
+            
+            plot.margin = margin(20, 20, 20, 20)
+        ) +
+        
+        labs(
+            y = "Allele Frequency"
+        )
+    return(o4)
     
 }
