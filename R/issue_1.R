bddab70030aeb749f3607271c12a056cb91b74e6
sfragkoul <sfragkoul@certh.gr>
2024-07-31 13:55:22 +0300

    add Freebayes

--- R/issue_1.R
+++ R/issue_1.R
@@ -839,3 +839,207 @@ ggsave(
     width = 16, height = 12, units = "in", dpi = 600
 )
 #Freebayes---------------------------------------------------------------------
+load_Freebayes_vcf <- function(path, merged_file){
+    #function to load caller vcf
+    Freebayes_somatic_vcf <- read.vcfR( paste0(path, merged_file, 
+                                               "_Freebayes_norm.vcf"), verbose = FALSE )
+    Freebayes_s0  = Freebayes_somatic_vcf |> vcfR::getFIX() |> as.data.frame() |> setDT()
+    #Freebayes_s1  = Freebayes_somatic_vcf |> extract_gt_tidy() |> setDT()
+    Freebayes_s2 = Freebayes_somatic_vcf |> extract_info_tidy() |> setDT()
+    Freebayes_s2 = Freebayes_s2[,c( "DP", "AF" )]
+    Freebayes_somatic = cbind(Freebayes_s0, Freebayes_s2)
+    return(Freebayes_somatic)
+}
+
+fp_snvs_Freebayes <- function(Freebayes_somatic_snvs, pick_gt, gt_all){
+    #find Freebayes FP variants
+    fp_var = define_fp(Freebayes_somatic_snvs, pick_gt)
+    fp_var$AF = as.numeric(fp_var$AF)
+    colnames(fp_var) = c("CHROM", "POS","ID", "Freebayes REF",	
+                         "Freebayes ALT", "Freebayes QUAL",	"Freebayes FILTER",
+                         "Freebayes DP", "Freebayes AF", "mut")
+    
+    #find DP of FP variants'  location in GT
+    tmp = gt_all[which(POS %in% unique(fp_var$POS))]
+    tmp = tmp[nchar(tmp$REF) == nchar(tmp$ALT)]
+    a = unique(tmp, by = "POS")
+    #to include the presence multiple variants in a POS
+    index = match(fp_var$POS, a$POS)
+    fp_var$`Ground Truth DP` = a[index]$DP
+    fp_var$`DP Percentage` = fp_var$`Freebayes DP`/fp_var$`Ground Truth DP`
+    fp_var$type = "FP"
+    return(fp_var)
+}
+
+fp_violin_plots_Freebayes <- function(q) {
+    #function to produce variants' barplots for coverage and AF
+    #q[which(q$`Freebayes ALT` == "")]$`Freebayes ALT` = NA
+    q$POS = as.numeric(q$POS)
+    q$`Ground Truth DP` = as.numeric(q$`Ground Truth DP`)
+    q$`Freebayes DP` = as.numeric(q$`Freebayes DP`)
+    
+    #DP plot
+    df = q[, c(
+        "POS", 
+        "Ground Truth DP",
+        "Freebayes DP"
+    ), with = FALSE] |>
+        unique() |>
+        
+        melt(id.vars = "POS", variable.factor = FALSE, value.factor = FALSE)
+    
+    o1 = ggplot(data = df) +
+        
+        geom_point(aes(x = variable, y = value, fill = variable),
+                   position = position_jitternormal(sd_x = .01, sd_y = 0),
+                   shape = 21, stroke = .1, size = 2.5) +
+        
+        geom_violin(aes(x = variable, y = value, fill = variable),
+                    width = .25, alpha = .5, outlier.shape = NA) +
+        
+        scale_fill_manual(
+            values = c(
+                "Ground Truth DP" = "#43ae8d",
+                "Freebayes DP"      = "#ae8d43"
+            )
+        ) +
+        
+        scale_x_discrete(
+            breaks = c("Ground Truth DP", "Freebayes DP"),
+            labels = c("Ground Truth", "Freebayes")
+        ) +
+        
+        scale_y_continuous(labels = scales::comma) +
+        
+        theme_minimal() +
+        
+        theme(
+            legend.position = "none",
+            
+            axis.title.x = element_blank(),
+            axis.title.y = element_text(face = "bold", size = 13),
+            axis.text.x = element_text(face = "bold", size = 13),
+            axis.text.y = element_text(face = "bold", size = 13),
+            
+            axis.line = element_line(),
+            axis.ticks = element_line(),
+            
+            panel.grid = element_blank(),
+            
+            plot.margin = margin(20, 20, 20, 20)
+        ) +
+        
+        labs(
+            y = "Coverage (No. of reads)"
+        )
+}
+
+fp_af_barplot <- function(q){
+    #FP AF plot
+    df = q[, c(
+        "POS",
+        "Freebayes AF"
+    ), with = FALSE] |>
+        unique() |>
+        
+        melt(id.vars = "POS", variable.factor = FALSE, value.factor = FALSE)
+    
+    o2 = ggplot(data = df[which(!is.na(value) & value != 0)]) +
+        
+        geom_point(aes(x = variable, y = value, fill = variable),
+                   position = position_jitternormal(sd_x = .01, sd_y = 0),
+                   shape = 21, stroke = .1, size = 2.5) +
+        
+        geom_boxplot(aes(x = variable, y = value, fill = variable),
+                     width = .25, alpha = .5, outlier.shape = NA) +
+        
+        scale_fill_manual(
+            values = c(
+                #"Ground Truth AF" = "#43ae8d",
+                "Freebayes AF"      = "#ae8d43"
+            )
+        ) +
+        
+        scale_x_discrete(
+            labels = c("Freebayes FP Variants")
+        ) +
+        
+        scale_y_continuous(labels = scales::percent, trans = "log10") +
+        
+        theme_minimal() +
+        
+        theme(
+            legend.position = "none",
+            
+            axis.title.x = element_blank(),
+            axis.title.y = element_text(face = "bold", size = 13),
+            axis.text.x = element_text(face = "bold", size = 13),
+            axis.text.y = element_text(face = "bold", size = 13),
+            
+            axis.line = element_line(),
+            axis.ticks = element_line(),
+            
+            panel.grid = element_blank(),
+            
+            plot.margin = margin(20, 20, 20, 20)
+        ) +
+        
+        labs(
+            y = "Allele Frequency"
+        )
+    return(o2)
+    
+}
+
+
+Freebayes_somatic <- load_Freebayes_vcf("results/", "Merged_auto")
+Freebayes_somatic_snvs <-select_snvs(Freebayes_somatic)
+
+
+#FP
+Freebayes_fp_var = fp_snvs_Freebayes(Freebayes_somatic_snvs, pick_gt, gt_all)
+
+fwrite(
+    Freebayes_fp_var, paste0("results/", "Merged_auto_", "Freebayes_", "snvs_FP.tsv"),
+    row.names = FALSE, quote = FALSE, sep = "\t"
+)
+
+#FN
+Freebayes_fn_var = define_fn(Freebayes_somatic_snvs, pick_gt)
+colnames(Freebayes_fn_var) = c("POS", "Ground Truth REF", "Ground Truth DP", 
+                               "Ground Truth ALT", "Count", "Ground Truth AF", "mut", "type")
+
+fwrite(
+    Freebayes_fn_var, paste0("results/", "Merged_auto_", "Freebayes_", "snvs_FN.tsv"),
+    row.names = FALSE, quote = FALSE, sep = "\t"
+)
+
+#Plots
+
+Freebayes_fp_plot1 <- fp_violin_plots_Freebayes(Freebayes_fp_var)
+Freebayes_fp_plot2 <- fp_af_barplot(Freebayes_fp_var)
+Freebayes_fn_plot1 <- fn_dp_barplot(Freebayes_fn_var, caller = "Freebayes")
+Freebayes_fn_plot2 <- fn_af_barplot(Freebayes_fn_var, caller = "Freebayes")
+
+Freebayes_multi1 = Freebayes_fp_plot1 + Freebayes_fp_plot2 +
+    
+    plot_layout(
+        widths = c(1, 1)
+    )
+
+Freebayes_multi2 = Freebayes_fn_plot1 + Freebayes_fn_plot2 +
+    
+    plot_layout(
+        widths = c(1, 1)
+    )
+
+Freebayes_multi3 = Freebayes_multi1 / Freebayes_multi2 &
+    
+    theme(
+        plot.margin = margin(10, 10, 10, 10)
+    )
+
+ggsave(
+    plot = Freebayes_multi3, filename = paste0("results/Plots/", "Merged_auto_", "Freebayes_snvs_FN_FP.png"),
+    width = 16, height = 12, units = "in", dpi = 600
+)
