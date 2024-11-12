4e67edb9781af761007e79108ed58bbdb90f3831
sfragkoul <sfragkoul@certh.gr>
2024-11-12 15:24:29 +0200

    make into function and transfer to main script

--- issue_4.R
+++ issue_4.R
@@ -83,7 +83,7 @@ define_fn <- function(caller, gt){
     return(fn_var)
 }
 
-categorize_fns <- function(caller, fn_var) { #!!!! NEW FUNCTION
+categorize_fns_gatk <- function(caller, fn_var) { #!!!! NEW FUNCTION
     #Function to identify FN categories
     
     caller$POS = as.numeric(caller$POS)
@@ -103,6 +103,28 @@ categorize_fns <- function(caller, fn_var) { #!!!! NEW FUNCTION
     return(fn_var)
 }
 
+categorize_fps_gatk <- function(pick_gt_stdz, fp_indels_gatk) { #!!!! NEW FUNCTION
+    #Function to identify FP categories
+    pick_gt_stdz$POS = as.numeric(pick_gt_stdz$POS)
+    fp_indels_gatk$POS = as.numeric(fp_indels_gatk$POS)
+    colnames(fp_indels_gatk) = c("CHROM", "POS", "ID", "REF", "ALT", "Mutect2 QUAL",
+                                 "Mutect2 FILTER", "key", "Indiv", "Mutect2 AD", 
+                                 "Mutect2 AF", "Mutect2 DP", "gt_F1R2", "gt_F2R1", 
+                                 "gt_FAD", "gt_GQ", "gt_GT", "gt_PGT", "gt_PID", 
+                                 "gt_PL" , "gt_PS", "gt_SB", "gt_GT_alleles", 
+                                 "mut", "Ground Truth DP","DP Percentage", "type")
+    #Same POS
+    same_POS <- merge(fp_indels_gatk, pick_gt_stdz, by = c("POS"))
+    fp_indels_gatk[, category := ifelse(POS %in% same_POS$POS, "diff REF", "not exist")]
+    
+    #Same POS & REF
+    same_POS_REF <- merge(fp_indels_gatk, pick_gt_stdz, by = c("POS", "REF"))
+    # Update only rows where POS and REF match
+    fp_indels_gatk[POS %in% same_POS_REF$POS & REF %in% same_POS_REF$REF, 
+                   category := "diff ALT"]
+    
+    return(fp_indels_gatk)
+}
 
 define_tp <- function(caller, gt){
     #TP Variants
--- thoughts.R
+++ thoughts.R
@@ -1,21 +1,25 @@
 
-categorize_fns <- function(caller, fn_var) {
-    #Function to identify FN categories
-    caller$POS = as.numeric(caller$POS)
-    fn_var$POS = as.numeric(fn_var$POS)
-    colnames(fn_var) = c("POS","REF", "Ground Truth DP",  "ALT",
-                         "Count", "Ground Truth AF","mut","type")
+categorize_fps_gatk <- function(pick_gt_stdz, fp_indels_gatk) { #!!!! NEW FUNCTION
+    #Function to identify FP categories
+    pick_gt_stdz$POS = as.numeric(pick_gt_stdz$POS)
+    fp_indels_gatk$POS = as.numeric(fp_indels_gatk$POS)
+    colnames(fp_indels_gatk) = c("CHROM", "POS", "ID", "REF", "ALT", "Mutect2 QUAL",
+                                 "Mutect2 FILTER", "key", "Indiv", "Mutect2 AD", 
+                                 "Mutect2 AF", "Mutect2 DP", "gt_F1R2", "gt_F2R1", 
+                                 "gt_FAD", "gt_GQ", "gt_GT", "gt_PGT", "gt_PID", 
+                                 "gt_PL" , "gt_PS", "gt_SB", "gt_GT_alleles", 
+                                 "mut", "Ground Truth DP","DP Percentage", "type")
     #Same POS
-    same_POS <- merge(fn_var, caller, by = c("POS"))
-    fn_var[, category := ifelse(POS %in% same_POS$POS, "diff REF", "not exist")]
+    same_POS <- merge(fp_indels_gatk, pick_gt_stdz, by = c("POS"))
+    fp_indels_gatk[, category := ifelse(POS %in% same_POS$POS, "diff REF", "not exist")]
     
     #Same POS & REF
-    same_POS_REF <- merge(fn_var, caller, by = c("POS", "REF"))
+    same_POS_REF <- merge(fp_indels_gatk, pick_gt_stdz, by = c("POS", "REF"))
     # Update only rows where POS and REF match
-    fn_var[POS %in% same_POS_REF$POS & REF %in% same_POS_REF$REF, 
-           category := "diff ALT"]
+    fp_indels_gatk[POS %in% same_POS_REF$POS & REF %in% same_POS_REF$REF, 
+                   category := "diff ALT"]
     
-    return(fn_var)
+    return(fp_indels_gatk)
 }
 
 
@@ -25,24 +29,6 @@ source("R/libraries.R")
 caller -> pick_gt_stdz
 fn_var -> fp_indels_gatk
 
-
-pick_gt_stdz$POS = as.numeric(pick_gt_stdz$POS)
-fp_indels_gatk$POS = as.numeric(fp_indels_gatk$POS)
-colnames(fp_indels_gatk) = c("CHROM", "POS", "ID", "REF", "ALT", "Mutect2 QUAL",
-                             "Mutect2 FILTER", "key", "Indiv", "Mutect2 AD", 
-                             "Mutect2 AF", "Mutect2 DP", "gt_F1R2", "gt_F2R1", 
-                             "gt_FAD", "gt_GQ", "gt_GT", "gt_PGT", "gt_PID", 
-                             "gt_PL" , "gt_PS", "gt_SB", "gt_GT_alleles", 
-                             "mut", "Ground Truth DP","DP Percentage", "type")
-#Same POS
-same_POS <- merge(fp_indels_gatk, pick_gt_stdz, by = c("POS"))
-fp_indels_gatk[, category := ifelse(POS %in% same_POS$POS, "diff REF", "not exist")]
-
-#Same POS & REF
-same_POS_REF <- merge(fp_indels_gatk, pick_gt_stdz, by = c("POS", "REF"))
-# Update only rows where POS and REF match
-fp_indels_gatk[POS %in% same_POS_REF$POS & REF %in% same_POS_REF$REF, 
-       category := "diff ALT"]
 
 
 fwrite(
