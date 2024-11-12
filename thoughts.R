47bd83aeea345a6c01606738f463a36011a5ba94
sfragkoul <sfragkoul@certh.gr>
2024-11-12 12:55:35 +0200

    thoughts that worked!

--- thoughts.R
+++ thoughts.R
@@ -1,30 +1,35 @@
 # Function to identify FN variants with specific categories
-categorize_fns <- function(caller, fn_var) {
-    
-    caller = Mutect2_indels 
-    fn_var = fn_indels_gatk
+# categorize_fns <- function(caller, fn_var) {
+#     
+#     caller = Mutect2_indels 
+#     fn_var = fn_indels_gatk
+#     caller$POS = as.numeric(caller$POS)  
+#     fn_var$POS = as.numeric(fn_var$POS) 
+#     colnames(fn_var) = c("POS","REF", "Ground Truth DP",  "ALT",
+#                          "Count", "Ground Truth AF","mut","type") 
+# 
+#     return()
+# }
+#  
 
-    return()
-}
- 
+#-------
+source("R/libraries.R")
 
-
-caller$POS = as.numeric(caller$POS)  
-fn_var$POS = as.numeric(fn_var$POS) 
-
-colnames(fn_var) = c("POS","REF", "Ground Truth DP",  "ALT",
-                                           "Count", "Ground Truth AF","mut","type") 
+caller <- fread("caller.tsv", header = TRUE, sep = "\t") 
+fn_var <- fread("fn_var.tsv", header = TRUE, sep = "\t") 
 
 
 #Same POS
 same_POS <- merge(fn_var, caller, by = c("POS"))
-fn_var[, category := ifelse(POS %in% same_POS$POS, "same POS", "diff POS")]
+fn_var[, category := ifelse(POS %in% same_POS$POS, "diff REF", "not exist")]
 
 #Same POS & REF
 same_POS_REF <- merge(fn_var, caller, by = c("POS", "REF"))
-fn_var[, category := ifelse(POS %in% same_POS_REF$POS &
-                            REF %in% same_POS_REF$REF, "same POS & REF",,
-                            category)]  # Keep existing category if it's "same POS"
+fn_var[POS %in% same_POS_REF$POS & REF %in% same_POS_REF$REF, 
+       category := "diff ALT"]
 
-#Same POS & REF & ALT
-same_POS_REF_ALT <- merge(fn_var, caller, by = c("POS", "REF", "ALT"))
+fwrite(
+    fn_var, "fn_var.tsv",
+
+        row.names = FALSE, quote = FALSE, sep = "\t"
+    )
