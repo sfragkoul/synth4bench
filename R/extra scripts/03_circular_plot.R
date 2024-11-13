c48a3b3ca92149f67080ea37ea462b89ab201f3f
Natasa Anastasiadou <141711668+natanast@users.noreply.github.com>
2024-11-13 12:40:26 +0200

    Add files via upload

--- R/extra scripts/03_circular_plot.R
+++ R/extra scripts/03_circular_plot.R
@@ -6,6 +6,7 @@ gc()
 
 
 # Libraries
+
 library(data.table)
 library(stringr)
 
@@ -14,9 +15,9 @@ library(stringr)
 
 tp = fread("GATK_indels_TP.tsv", sep = "\t")
 
-fp = fread("GATK_indels_FP.tsv", sep = "\t")
+fp = fread("fp_var.tsv", sep = "\t")
 
-fn = fread("GATK_indels_FN.tsv", sep = "\t")
+fn = fread("fn_var.tsv", sep = "\t")
 
 
 
@@ -28,34 +29,29 @@ tp$ALT_len <- str_length(tp$ALT)
 
 tp$len_dif <- tp$ALT_len - tp$REF_len
 
+tp$category <- "not exist"
 
 
 
-fp = fp[, .(POS, `Mutect2 REF`, `Mutect2 ALT`, type)]
+fp = fp[, .(POS, REF, ALT, type, category)]
 
-fp$REF_len <- str_length(fp$`Mutect2 REF`)
+fp$REF_len <- str_length(fp$REF)
 
-fp$ALT_len <- str_length(fp$`Mutect2 ALT`)
+fp$ALT_len <- str_length(fp$ALT)
 
 fp$len_dif <- fp$ALT_len - fp$REF_len
 
 
-colnames(fp) <- c("POS", "REF", "ALT", "type", "REF_len", "ALT_len", "len_dif")
 
+fn = fn[, .(POS, REF, ALT, type, category)]
 
+fn$REF_len <- str_length(fn$REF)
 
-
-fn = fn[, .(POS, `Ground Truth REF`, `Ground Truth ALT`, type)]
-
-fn$REF_len <- str_length(fn$`Ground Truth REF`)
-
-fn$ALT_len <- str_length(fn$`Ground Truth ALT`)
+fn$ALT_len <- str_length(fn$ALT)
 
 
 fn$len_dif <- fn$ALT_len - fn$REF_len
 
-
-colnames(fn) <- c("POS", "REF", "ALT", "type", "REF_len", "ALT_len", "len_dif")
 
 
 # Combine the datasets 
@@ -65,6 +61,7 @@ data = rbind(tp, fp)
 df = rbind(data, fn)
 
 
+colnames(df) <- c("POS", "REF", "ALT", "Type",  "REF_len", "ALT_len", "len_dif", "Category")
 
 
 # plot ---------
@@ -77,70 +74,82 @@ library(colorspace)
 # Adjust data so that each type has its own y-offset
 df <- df |>
     mutate(y_cycle = case_when(
-        type == "FP" ~ len_dif + 50,   # Shift FN cycle outward
-        type == "TP" ~ len_dif + 25,   # Shift FP cycle to middle
-        type == "FN" ~ len_dif         # Keep TP at the center
+        Type == "FN" ~ len_dif + 50,   # Shift FN cycle outward
+        Type == "FP" ~ len_dif + 25,   # Shift FP cycle to middle
+        Type == "TP" ~ len_dif         # Keep TP at the center
     ))
 
 
 
+# Ensure 'category' is a factor
+df$Category <- factor(df$Category, levels = c("not exist", "diff REF", "diff ALT"))
 
-p = ggplot(df, aes(x = POS, y = y_cycle)) +
+
+p <- ggplot(df, aes(x = POS, y = y_cycle)) +
     
     # Lollipop segments: start each from the respective baseline to the point
     geom_segment(
         aes(x = POS, xend = POS,
-            y = ifelse(type == "FP", 50, ifelse(type == "TP", 25, 0)),
+            y = ifelse(Type == "FN", 50, ifelse(Type == "FP", 25, 0)),
             yend = y_cycle),
         color = "grey75", linewidth = 0.25, lineend = "round"
     ) +
-
-    # add connecting line
-    #geom_line(aes(color = type), linewidth = 0.25) + 
-
+    
     # Dashed lines for separation of each cycle level
     geom_hline(yintercept = 50, color = "grey40") +
     geom_hline(yintercept = 25, color = "grey40") +
     geom_hline(yintercept = 0,  color = "grey40") +
     
+    # # Add points at the end of each segment for the lollipop head
+    # geom_point(aes(fill = Type, color = Type, shape = Category), size = 2.3, stroke = .15) +
+    # 
+
     # Add points at the end of each segment for the lollipop head
-    geom_point(aes(fill = type, color = type), size = 1.5, shape = 21, stroke = .15) +
+    geom_point(aes(fill = Type, color = Type, shape = Category, 
+                   size = ifelse(Category == "not exist", 1.5, 3)), # Increase size for specific categories
+               stroke = .15) +
     
+    scale_size_identity() +
+    
+    # Define specific shapes for each category level
+    scale_shape_manual(values = c("diff REF" = 23, "diff ALT" = 24, "not exist" = 21)) +
     
     
     # Define custom colors for each type
     scale_fill_manual(values = c("TP" = "#a78d95", "FP" = "#ae4364", "FN" = "#43ae8d")) +
-    scale_color_manual(values = c("TP" = "#a78d95", "FP" = "#ae4364", "FN" = "#43ae8d") |> darken(.25), guide = "none") +
     
+    scale_color_manual(values = c("TP" = "#a78d95", "FP" = "#ae4364", "FN" = "#43ae8d") |> darken(.25)) +
+    
+
+    # Customize the x-axis and radial coordinates
     scale_x_continuous(breaks = c(0, 4751, 9503, 14255, 19007), limits = c(0, 19007)) +
     
+    coord_radial(start = pi / 2.5, inner.radius = .25, end = 2.6 * pi) +
     
-    coord_radial(start = pi / 2.5, inner.radius = .5, end = 2.6 * pi) +
+
+    # Remove legend for size if unnecessary
+    guides(size = "none") +
     
     theme_minimal() +
     
     theme(
         axis.text.y = element_blank(),
-        
         panel.grid.major = element_line(linewidth = 0.35),
         panel.grid.minor = element_blank(),
-        
         plot.margin = margin(20, 20, 20, 20),
-        
         plot.title = element_text(size = 15, face = "bold", hjust = 0.5)
-        
     ) +
     
+    # Add labels for the plot
     labs(
         title = "Ground Truth vs Mutect2 INDELS",
         y = "REF vs ALT Length Difference",
-        x = "Chromosomal Position",
-        fill = "Type"
+        x = "Chromosomal Position"
+        # color = "Type"
     )
-
 p
 
 ggsave(
-    plot = p, filename = "Rplot_4.jpeg",
-    width = 12, height = 12, units = "in", dpi = 600
+    plot = p, filename = "Rplot_3.jpeg",
+    width = 14, height = 12, units = "in", dpi = 600
 )
