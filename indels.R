5e0a7761e5c34ba710c859715dc8a34f5d08b86b
sfragkoul <sfragkoul@certh.gr>
2024-11-14 16:42:28 +0200

    start working on Freebayes

--- indels.R
+++ indels.R
@@ -1,6 +1,7 @@
 source("R/libraries.R")
 source("R/common_helpers.R")
-source("R/helpers_gatk.R")
+#source("R/helpers_gatk.R")
+source("R/helpers_freebayes.R")
 
 pick_gt_stdz = gt_stdz_indels("results", "Merged")
 
@@ -29,13 +30,45 @@ call_fp_indels_gatk <- function(path, merged_file){
     return(fp_indels_gatk_categories)
 }
 new_fp = call_fp_indels_gatk("results", "Merged")
+#------------------------------------------------------------------------------
 
-# fwrite(
-#     new_fn, paste0("fn_var_new.tsv"),
-#     row.names = FALSE, quote = FALSE, sep = "\t"
-# )
-# 
-# fwrite(
-#     new_fp, paste0("fp_var_new.tsv"),
-#     row.names = FALSE, quote = FALSE, sep = "\t"
-# )
+final_tp_indels_freebayes <- function(path, merged_file, pick_gt_stdz){
+    #function to identify TP indels
+    Freebayes_somatic_indels <- load_Freebayes_vcf(path, merged_file) |> select_indels()
+    tp_var = define_tp(Freebayes_somatic_indels, pick_gt_stdz)
+    return(tp_var)
+}
+
+tp_indels_freebayes = final_tp_indels_freebayes("results", "Merged", pick_gt_stdz)
+
+
+
+#FN
+call_fn_indels_Freebayes <- function(path, merged_file, pick_gt_stdz){
+    #function to output categorized FN indels
+    fn_indels_Freebayes = final_fn_indels_Freebayes(path, merged_file, pick_gt_stdz)
+    Freebayes_somatic = load_Freebayes_vcf(path, merged_file)
+    Freebayes_indels = select_indels(Freebayes_somatic)
+    fn_indels_Freebayes_categories = categorize_fns_Freebayes(Freebayes_indels, fn_indels_Freebayes)
+    
+    return(fn_indels_Freebayes_categories)
+}
+new_fn = call_fn_indels_Freebayes("results", "Merged")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
